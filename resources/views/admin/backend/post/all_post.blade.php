b3378a428729e78a08ba00e5590a752bb9fb0249
easylearningbd <elebdsoft@gmail.com>
2025-04-09 01:35:45 +0600

    b81 Blog Post Section Setup Part 4

--- resources/views/admin/backend/post/all_post.blade.php
+++ resources/views/admin/backend/post/all_post.blade.php
@@ -40,7 +40,7 @@
                 <td> <img src="{{ asset($item->image) }}" style="width:70px; height:40px;"> </td>
                 <td>{!! Str::limit($item->long_descp, 50, '...')  !!}</td>
                 <td>
-    <a href="{{ route('edit.review',$item->id) }}" class="btn btn-success btn-sm">Edit</a>  
+    <a href="{{ route('edit.blog.post',$item->id) }}" class="btn btn-success btn-sm">Edit</a>  
     <a href="{{ route('delete.review',$item->id) }}" class="btn btn-danger btn-sm" id="delete">Delete</a>              
 
                 </td>
